df3e2a5bda96c6aafc48d84dae9b2d072583fc4c
Brett Sloan <brett.sloan@hotmail.com>
2019-11-27 23:59:50 -0500

    Portfolio V2 100% - Mobile friendly - Fixed Javascript - Fixed small stuff

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,23 +1,30 @@
 $(document).ready(function () {
 
-	// Top Links
+	// Bottom Links & Bar
+    //Click link for active class
 	$(".a-link").on('click', function() {
         var x = $(this).attr("alt");
+
+        //remove active & active-bar from all a-links
         $(".a-link").removeClass("active");
         $(".a-link").removeClass("active-bar");
+
+        //cycle through a-links to find x, add active & active-bar
         $(".a-link").each(function () {
             if (($(this).attr("alt") == x) && $(this).hasClass("link-line")) {
-                
                 $(this).addClass("active-bar");
             } else if ($(this).attr("alt") == x) {
                 $(this).addClass("active");
             }
         });
+
+        //fade out on screen content and fade in linked content
         x = x.slice(1);
         $(".switch").fadeOut(0);
         $("div#" + x).fadeIn(200);
-
     });
+
+    //Left & Right arrow keys to cycle through content
     $(document).keydown(function (e) {
         switch (e.which) {
             case 37: //left
@@ -84,34 +91,26 @@ $(document).ready(function () {
 
     });
 
-    $(".padding").mouseenter(function () {
+    //On mouse hover, toggle grid backgrounds to fade out and information to fade in
+    $(".padding-none").mouseenter(function () {
         $(".card", this).fadeTo("slow", 0);
         $(".card-body", this).fadeTo("slow", 1);
     });
-    $(".padding").mouseleave(function () {
+    $(".padding-none").mouseleave(function () {
         $(".card", this).fadeTo("slow", 1);
         $(".card-body", this).fadeTo("slow",0);
     });
 
 	//NavBar Corrections
-	$(".mt-1").on('click', function() {
-		$(".navbar-collapse").addClass("open");
-		var value = $('body').css('padding-top');
-		if (value === '20px') {
-            $('body').css('padding-top', '+=120');
-            $(".link-bar").css('bottom', '40px');
-		} else {
-            $('body').css('padding-top', '20px');
-            $(".link-bar").css('bottom', '+=120');
-		}
-	});
-
-	$(document).on('click', function (event) {
-		var clickover = $(event.target);
-		var _opened = $(".navbar-collapse").hasClass("open");
-		if (_opened === true && !clickover.hasClass("mt-1")) {
-			$(".mt-1").click();
-			$(".navbar-collapse").removeClass("open");
+    $(".mt-1").on('click', function () {
+        $(this).toggleClass("change");
+        var value = $(".link-bar").css("bottom");
+        if (value === "40px") {
+            $(".main-wrapper").css("max-height", "calc(100vh - 195px)");
+            $(".link-bar").css("bottom", "165px");
+        } else {
+            $(".main-wrapper").css("max-height", "calc(100vh - 70px");
+            $(".link-bar").css("bottom", "40px");
 		}
 	});
 
